bcebfe58aeaa1eb833928709a44da3135dd4e43a
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2025-12-04 17:19:35 -0300

    Fix flickering and pupil range

--- ESP32-2432S028/ESP32-2432S028.ino
+++ ESP32-2432S028/ESP32-2432S028.ino
@@ -3,6 +3,7 @@
 #include <TFT_eSPI.h>
 
 TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite eyeSprite = TFT_eSprite(&tft);
 
 float lx = 0, ly = 0;
 float rx = 0, ry = 0;
@@ -11,15 +12,17 @@ float expr = 0;
 String logBuffer;
 const int LOG_LINES = 6;  
 String logLines[LOG_LINES];
+bool logsChanged = true;
 
 
 void pushLog(const String &msg) {
   for (int i = LOG_LINES - 1; i > 0; i--) { logLines[i] = logLines[i - 1]; }
   logLines[0] = msg;
+  logsChanged = true;
 }
 
 void drawEyes() {
-  tft.fillRect(0, 0, 320, 160, TFT_BLACK); // Screen is 320x240. Using top 160 for eyes.
+  eyeSprite.fillSprite(TFT_BLACK);
   int eyeW = 130;
   int eyeH = 120;
   int padding = 15;
@@ -27,26 +30,29 @@ void drawEyes() {
   int ly0 = padding;
   int rx0 = 160 + padding;
   int ry0 = padding;
-  int pupilR = 16;
+  int pupilR = 20;
   int moveRange = 25;
   int eyelidDrop = (int)(expr * 40);
-  tft.fillRoundRect(lx0, ly0 + eyelidDrop, eyeW, eyeH - eyelidDrop, 20, TFT_WHITE);
+  eyeSprite.fillRoundRect(lx0, ly0 + eyelidDrop, eyeW, eyeH - eyelidDrop, 20, TFT_WHITE);
   int lcx = lx0 + eyeW/2 + lx * moveRange;
   int lcy = ly0 + eyeH/2 + ly * moveRange + eyelidDrop;
-  tft.fillCircle(lcx, lcy, pupilR, TFT_BLACK);
-  tft.fillRoundRect(rx0, ry0 + eyelidDrop, eyeW, eyeH - eyelidDrop, 20, TFT_WHITE);
+  eyeSprite.fillCircle(lcx, lcy, pupilR, TFT_BLACK);
+  eyeSprite.fillRoundRect(rx0, ry0 + eyelidDrop, eyeW, eyeH - eyelidDrop, 20, TFT_WHITE);
   int rcx = rx0 + eyeW/2 + rx * moveRange;
   int rcy = ry0 + eyeH/2 + ry * moveRange + eyelidDrop;
-  tft.fillCircle(rcx, rcy, pupilR, TFT_BLACK);
+  eyeSprite.fillCircle(rcx, rcy, pupilR, TFT_BLACK);
+  eyeSprite.pushSprite(0, 0);
 }
 
 void drawLogs() {
+  if (!logsChanged) return;
   int y0 = 170;
   tft.fillRect(0, 160, 320, 80, 0x2104);
   tft.setCursor(5, y0);
   tft.setTextColor(TFT_CYAN);
   tft.setTextSize(1);
   for (int i = LOG_LINES - 1; i >= 0; i--) { tft.println(logLines[i]); }
+  logsChanged = false;
 }
 
 void setup() {
@@ -54,6 +60,7 @@ void setup() {
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
+  eyeSprite.createSprite(320, 160);
   pushLog("Boot OK");
 }
 
@@ -70,10 +77,10 @@ void loop() {
           int idx4 = incoming.indexOf(',', idx3+1);
           int idx5 = incoming.indexOf(',', idx4+1);
           if (idx1 > 0 && idx5 > idx4) {
-            lx = incoming.substring(idx1+1, idx2).toFloat();
-            ly = incoming.substring(idx2+1, idx3).toFloat();
-            rx = incoming.substring(idx3+1, idx4).toFloat();
-            ry = incoming.substring(idx4+1, idx5).toFloat();
+            lx = 2*incoming.substring(idx1+1, idx2).toFloat();
+            ly = -2*incoming.substring(idx2+1, idx3).toFloat();
+            rx = 2*incoming.substring(idx3+1, idx4).toFloat();
+            ry = -2*incoming.substring(idx4+1, idx5).toFloat();
             expr = incoming.substring(idx5+1).toFloat();
           }
         } else { pushLog(incoming); }
